0d0a7c3b1ce76efd58e8c5ef464d8d6fb2ee2360
shyamshr93 <shyamshr93@gmail.com>
2021-04-28 22:05:13 +0530

    added slyabus

--- js/notes.js
+++ js/notes.js
@@ -10,36 +10,45 @@ let sub_id = params.get("id");
 if (sub_id === null)
     sub_id = "BCA 101"
 var selectedSem = [];
+var sly_index = 0;
+var course_code = sub_id.replace("BCA ", "20")
 
 document.getElementById("sub-code").innerText = sub_id;
 
 if (sem1.includes(sub_id)) {
     document.getElementById("sem-title").innerText = "Semester 1";
     selectedSem = sem1;
+    sly_index = 0;
 }
 else if (sem2.includes(sub_id)) {
     document.getElementById("sem-title").innerText = "Semester 2";
     selectedSem = sem2;
+    sly_index = 1;
 }
 else if (sem3.includes(sub_id)) {
     document.getElementById("sem-title").innerText = "Semester 3";
     selectedSem = sem3;
+    sly_index = 2;
 }
 else if (sem4.includes(sub_id)) {
     document.getElementById("sem-title").innerText = "Semester 4";
     selectedSem = sem4;
+    sly_index = 3;
 }
 else if (sem5.includes(sub_id)) {
     document.getElementById("sem-title").innerText = "Semester 5";
     selectedSem = sem5;
+    sly_index = 4;
 }
 else if (sem6.includes(sub_id)) {
     document.getElementById("sem-title").innerText = "Semester 6";
     selectedSem = sem6;
+    sly_index = 5;
 }
 else {
     document.getElementById("sem-title").innerText = "Semester 1";
     selectedSem = sem1;
+    sly_index = 0;
 }
 
 
@@ -97,6 +106,47 @@ $.getJSON("https://raw.githubusercontent.com/Xbotics7/Nomtes/master/assets/nomte
     document.getElementById("sub-title").innerText = res;
     notes_collec = notesData["Notes"]
     notes_collec_object = Object.keys(notes_collec);
+
+
+    //Gettting Slyabus
+
+
+})
+
+$.getJSON("https://raw.githubusercontent.com/Xbotics7/bruh/main/course.json", function (data) {
+    var semsly = data[0]["semester"][sly_index]
+
+    var semsly_obj = Object.keys(semsly["subjects"]);
+
+    for (i = 0; i < semsly_obj.length; i++) {
+        var bruh = semsly["subjects"][semsly_obj[i]];
+
+        var notes = Object.keys(bruh);
+        var sly = bruh[notes[2]]
+        var sly_obj = Object.keys(sly);
+        console.log(course_code)
+        if (bruh[notes[1]].toString() === course_code) {
+            for (j = 0; j < sly_obj.length; j++) {
+                var slynew = sly[sly_obj[j]];
+                var title = slynew["title"]
+                var chap = slynew["chapters"]
+
+                document.getElementById("course-sly").innerHTML += ` <div class="sly-cont">
+                <h2 class="sly-unit">
+                    ${sly_obj[j].replace("_", "-")}
+                </h2>
+                <h2 class="sly-title">
+                    ${title}
+                </h2>
+                <p class="sly-chap">
+                    ${chap}
+                </p>
+            </div>`
+            }
+            return;
+        }
+
+    }
 })
 
 function openNotes() {
